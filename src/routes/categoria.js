9c6741b92504478abe781923ce572012f9256925
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-09-17 08:15:42 -0300

    updateRoutesCategoria

--- src/routes/categoria.js
+++ src/routes/categoria.js
@@ -4,11 +4,10 @@ const categoria = require('../controllers/categoria')
 
 
 router.get('/categorias', categoria.showAll)
-/* router.get('/categorias/:id', categoria.showOne)
-router.get('/categorias', categoria.showAll)
 router.post('/categorias', categoria.create)
-router.patch('/categorias/:id', categoria.update)
-router.delete('/categorias/:id', categoria.destroy) */
+router.get('/categorias/:id', categoria.showOne)
+router.patch('/categorias/:id', categoria.edit)
+router.delete('/categorias/:id', categoria.destroy)
 
 module.exports = router
 
