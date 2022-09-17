2d00251cc03f9a176f07ccaba877fd43bd748c70
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-09-17 16:41:27 -0300

    ImplementedEstoqueEndPoints GET PATCH DELETE

--- src/controllers/estoque.js
+++ src/controllers/estoque.js
@@ -1,4 +1,4 @@
-const { Estoque, Produto } = require('../database/models')
+const { Estoque } = require('../database/models')
 
 const estoqueController = {
   showOne: async (req, res) => {
@@ -16,21 +16,40 @@ const estoqueController = {
   },
   edit: async (req, res) => {
     const { id } = req.params
+    const {
+      idProduto,
+      quantiade,
+      reserva,
+      status
+    } = req.body
+    if (status && status !== 0 && status !== 1) {
+      return res.status(500).json({ Message: 'Status must be 0 or 1' })
+    }
     try {
+      const estoque = await Estoque.findOne({ where: { idProduto: id } })
 
+      if (!estoque) {
+        return res.status(404).json({ Message: 'Inventory Not Found' })
+      }
+      await Estoque.update({
+        idProduto,
+        quantiade,
+        reserva,
+        status
+      },
+        {
+          where: { idProduto: id }
+        })
+      return res.status(200).json({ Message: 'Inventory Updated' })
     } catch (error) {
       console.log(error)
       return res.status(500).json({ Message: 'Bad Request' })
     }
   },
-  destroy: async (req, res) => {
-    const { id } = req.params
-    try {
+  destroy: async (_req, res) => {
 
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json({ Message: 'Bad Request' })
-    }
+    return res.status(501).json({ Message: 'Not Implemented' })
+
   }
 }
 
--- src/routes/estoque.js
+++ src/routes/estoque.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const estoque = require('../controllers/estoque')
 
 router.get('/produtos/:id/estoque', estoque.showOne)
-router.patch('/prodtuos/:id/estoque', estoque.edit)
-router.delete('/prodtuos/:id/estoque', estoque.destroy)
+router.patch('/produtos/:id/estoque', estoque.edit)
+router.delete('/produtos/:id/estoque', estoque.destroy)
 
 module.exports = router
 
