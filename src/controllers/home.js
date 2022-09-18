799b4f50f0a2829ef86188df6d892257ac7f030f
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-09-18 08:15:59 -0300

    UpdatedHomeController

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,14 @@
 const homeController = {
   index: async (req, res) => {
-    res.status(200).json({ Message: 'Hello World' })
+    res.status(200).json({
+      Developer: 'João Lucas Nascimento Andrade',
+      GitHubRepository: 'https://github.com/Jlucas93/apiJest',
+      Routes: {
+        categorias: `http://localhost:${process.env.SERVER_PORT || 3000}/categorias`,
+        produtos: `http://localhost:${process.env.SERVER_PORT || 3000}/produtos`,
+        estoque: `http://localhost:${process.env.SERVER_PORT || 3000}/produtos/1/estoque`
+      }
+    })
   }
 }
 module.exports = homeController
